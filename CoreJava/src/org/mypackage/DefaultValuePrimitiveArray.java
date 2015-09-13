fc6eeded837ed3eb33158188aaee191371ba3450
Kawser Alam <kaiseralam@msn.com>
2015-09-13 10:46:22 -0400

    Default value for primitive array

---
+++ CoreJava/src/org/mypackage/DefaultValuePrimitiveArray.java
@@ -0,0 +1,41 @@
+package org.mypackage;
+
+public class DefaultValuePrimitiveArray {
+
+	public static void main(String[] args) {
+
+		String[] s = new String[3];
+		System.out.println("Default value for String array: " + s[1]);
+		
+		int[] i = new int[3];
+		System.out.println("Default value for Integer array: " + i[1]);
+		
+		double[] d = new double[3];
+		System.out.println("Default value for Double array: " + d[1]);
+		
+		char[] c = new char[3];
+		System.out.println("Default value for Character array: " + c[1]);
+		
+		byte[] b = new byte[3];
+		System.out.println("Default value for Byte array: " + b[1]);
+		
+		long[] l = new long[3];
+		System.out.println("Default value for Long array: " + l[1]);
+		
+		short[] ss = new short[3];
+		System.out.println("Default value for Short array: " + ss[1]);
+		
+		float[] f = new float[3];
+		System.out.println("Default value for Float array: " + f[1]);
+		
+		boolean[] bb = new boolean[3];
+		System.out.println("Default value for Boolean array: " + bb[1]);
+
+
+
+		
+		
+	}
+
+
+}
